d411689e0b2de695678379e3f1193c1576c25979
Kurt Taschl <kurt.taschl@gmail.com>
2005-07-19 13:38:42 +0000

    did some changes to be support ssl note that not all needed changes are done atm - still some work needed

--- ftp4che/src/java/org/ftp4che/util/SocketProvider.java
+++ ftp4che/src/java/org/ftp4che/util/SocketProvider.java
@@ -11,12 +11,20 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SocketChannel;
 
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+
 
 import org.apache.log4j.Logger;
 
 public class SocketProvider {
 
-	SocketChannel socketChan = null;
+	private int sslMode = 0; // 0 = no ssl ; 1 = implicit ssl ; 2 = auth tls ; 3 = auth ssl
+    SocketChannel socketChan = null;
+    SSLEngineResult res;
+    SSLEngine sslEngine;
 	Logger log = Logger.getLogger(SocketProvider.class.getName());
 	
 	ByteBuffer clientIn, clientOut, cTOs, sTOc, wbuf;
@@ -24,10 +32,20 @@ public class SocketProvider {
 	public SocketProvider() throws IOException {
 		socketChan = SocketChannel.open();
 	}
+    
+    public SocketProvider( int sslMode ) throws IOException {
+        socketChan = SocketChannel.open();
+        setSSLMode( sslMode );
+    }
 	
 	public SocketProvider( SocketChannel socketChan ) {
 		this.socketChan = socketChan;
 	}
+    
+    public SocketProvider( SocketChannel socketChan, int sslMode ) {
+        this.socketChan = socketChan;
+        setSSLMode( sslMode );
+    }
 	
 	public boolean connect( SocketAddress remote ) throws IOException {
 		return socketChan.connect(remote);
@@ -47,7 +65,13 @@ public class SocketProvider {
 	}
 	
 	public void close() throws IOException {
-		socketChan.close();
+        if ( this.sslMode > 0 ) {
+            sslEngine.closeOutbound();
+            clientOut.clear();
+            socketChan.write(wrap(clientOut));
+            socketChan.close();
+        } else        
+            socketChan.close();
 	}
 	
 	public SelectableChannel configureBlocking( boolean blockingState ) throws IOException {
@@ -56,6 +80,8 @@ public class SocketProvider {
 	
 
 	public int write(ByteBuffer src) throws IOException {
+        if ( this.sslMode > 0 )
+            return socketChan.write(wrap(src));
 		return socketChan.write(src);
 	}
 	
@@ -67,4 +93,44 @@ public class SocketProvider {
 	public String toString() {
 		return socketChan.socket().getInetAddress().getHostAddress() + ":" + socketChan.socket().getPort();
 	}
+
+    /**
+     * @return Returns the sslMode.
+     */
+    public int getSSLMode() {
+        return sslMode;
+    }
+
+    /**
+     * @param sslMode The sslMode to set.
+     */
+    public void setSSLMode(int sslMode) {
+        this.sslMode = sslMode;
+    }
+    
+    private void createBuffers(SSLSession session) {
+        
+        int appBufferMax = session.getApplicationBufferSize();
+        int netBufferMax = session.getPacketBufferSize();
+    
+        clientIn = ByteBuffer.allocate(65536);
+        clientOut = ByteBuffer.allocate(appBufferMax);
+        wbuf = ByteBuffer.allocate(65536);
+    
+        cTOs = ByteBuffer.allocate(netBufferMax);
+        sTOc = ByteBuffer.allocate(netBufferMax);
+    
+    }
+    
+    private synchronized ByteBuffer wrap(ByteBuffer b) throws SSLException {
+        return null;
+    }
+    
+    private synchronized ByteBuffer unwrap(ByteBuffer b) throws SSLException {
+        return null;
+    }
+    
+    public boolean isInboundDone() {
+        return sslEngine.isInboundDone();
+    }
 }
