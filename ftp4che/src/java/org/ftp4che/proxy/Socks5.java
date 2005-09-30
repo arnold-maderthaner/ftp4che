5c5bc193bb069d2da4fd8c588061e44aed963794
Kurt Taschl <kurt.taschl@gmail.com>
2005-09-30 16:41:20 +0000

    source maintance, fixed some little issues

--- ftp4che/src/java/org/ftp4che/proxy/Socks5.java
+++ ftp4che/src/java/org/ftp4che/proxy/Socks5.java
@@ -1,9 +1,8 @@
 package org.ftp4che.proxy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -14,21 +13,52 @@ import org.ftp4che.exception.ProxyConnectionException;
 
 public class Socks5 implements Proxy {
     
+    // constants
+    static final int PROTOCOL_VERSION         = 5;
+    static final int DEFAULT_PORT             = 1080;
+    static final int DEFAULT_TIMEOUT          = 20000;
+
+    static final int NO_AUTH                  = 0;
+    static final int GSSAPI                   = 1;
+    static final int USER_PASS                = 2;
+
+    static final int CONNECT                  = 1;
+    static final int BIND                     = 2;
+    static final int UDP                      = 3;
+
+    static final int IPV4                     = 1;
+    static final int DOMAIN_NAME              = 3;
+    static final int IPV6                     = 4;
+    
+    static final int REQUEST_OK               = 0;
+    static final int GENERAL_FAILURE          = 1;
+    static final int NOT_ALLOWED              = 2;
+    static final int NET_UNREACHABLE          = 3;
+    static final int HOST_UNREACHABLE         = 4;
+    static final int CONNECTION_REFUSED       = 5;
+    static final int TTL_EXPIRED              = 6;
+    static final int CMD_NOT_SUPPORTED        = 7;
+    static final int ADDR_TYPE_NOT_SUPPORTED  = 8;
+    
 	public static final Logger log = Logger.getLogger(Socks5.class.getName());
 	
     private String host;
     private String user;
     private String pass;
-    private int port;
+    private int port = DEFAULT_PORT;
     private int primaryConnectionPort;
-    
+    private int timeout = DEFAULT_TIMEOUT;
     private Socket socket = null;
 
-    public Socks5(String proxyHost, int proxyPort) {
-    	this(proxyHost, proxyPort, null, null);
+    public Socks5(String proxyHost, String proxyUser, String proxyPass) {
+        this(proxyHost, -1, -1, proxyUser, proxyPass);
     }
     
     public Socks5(String proxyHost, int proxyPort, String proxyUser, String proxyPass) {
+        this(proxyHost, proxyPort, -1, proxyUser, proxyPass);
+    }
+    
+    public Socks5(String proxyHost, int proxyPort, int timeout, String proxyUser, String proxyPass) {
         setHost(proxyHost);
         setPort(proxyPort);
         setUser(proxyUser);
@@ -62,12 +92,12 @@ public class Socks5 implements Proxy {
         byte[] response = new byte[2];
         byte methodCount = 2;
         
-        requestPacket[0] = 5; // means socks5 (field VN)
+        requestPacket[0] = PROTOCOL_VERSION; // means socks5 (field VN)
         requestPacket[1] = methodCount; // methods count (field NMETHODS)
-        requestPacket[2] = 0; // X'00' NO AUTHENTICATION REQUIRED
-//        requestPacket[3] = 1; // X'01' GSSAPI
+        requestPacket[2] = NO_AUTH; // X'00' NO AUTHENTICATION REQUIRED
+//        requestPacket[3] = GSSAPI; // X'01' GSSAPI
         if (getUser() != null && getPass() != null)
-        	requestPacket[3] = 2; // X'02' USERNAME/PASSWORD
+        	requestPacket[3] = USER_PASS; // X'02' USERNAME/PASSWORD
 
         try {
             connectToProxy();
@@ -97,10 +127,10 @@ public class Socks5 implements Proxy {
     	byte[] requestPacket = new byte[1024];
     	byte[] response = new byte[10];
     	
-    	requestPacket[0] = 5; // means socks5 (field VN)
-    	requestPacket[1] = 1; // connect (field CMD)
+    	requestPacket[0] = PROTOCOL_VERSION; // means socks5 (field VN)
+    	requestPacket[1] = CONNECT; // connect (field CMD)
     	requestPacket[2] = 0; // reserved (field RSV)
-    	requestPacket[3] = 1; // IPv4 (field ATYP)
+    	requestPacket[3] = IPV4; // IPv4 (field ATYP)
     	
         // adding the host adress bytes to the packet (field DST.ADDR)
         System.arraycopy(hostbytes, 0, requestPacket, 4, 4);
@@ -108,47 +138,58 @@ public class Socks5 implements Proxy {
         requestPacket[8] = (byte) ( getPrimaryConnectionPort() >> 8 ); // (field DST.PORT)
         requestPacket[9] = (byte) ( getPrimaryConnectionPort() & 0x00ff ); // (field DST.PORT)
         
+        OutputStream out = null;
+        InputStream in = null;
         try {
-            this.socket.getOutputStream().write(requestPacket, 0, 10);
-            this.socket.getInputStream().read(response, 0, 10);
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+            
+            out.write(requestPacket, 0, 10);
+            in.read(response, 0, 10);
         }catch(IOException ioe) {
             throw new ProxyConnectionException(-2, "SOCK5 - IOException: " + ioe.getMessage());
         }
         
         ProxyConnectionException pce = null;
         switch (response[1]) {
-            case 0:
+            case REQUEST_OK:
                 break; // request successfull
-            case 1:
-                pce = new ProxyConnectionException(1, "SOCKS5 general SOCKS server failure");
+            case GENERAL_FAILURE:
+                pce = new ProxyConnectionException(GENERAL_FAILURE, "SOCKS5 general SOCKS server failure");
                 break;
-            case 2:
-                pce = new ProxyConnectionException(2, "SOCKS5 connection not allowed by ruleset");
+            case NOT_ALLOWED:
+                pce = new ProxyConnectionException(NOT_ALLOWED, "SOCKS5 connection not allowed by ruleset");
                 break;
-            case 3:
-                pce = new ProxyConnectionException(3, "SOCKS5 Network unreachable");
+            case NET_UNREACHABLE:
+                pce = new ProxyConnectionException(NET_UNREACHABLE, "SOCKS5 Network unreachable");
                 break;
-            case 4:
-                pce = new ProxyConnectionException(4, "SOCKS5 Host unreachable");
+            case HOST_UNREACHABLE:
+                pce = new ProxyConnectionException(HOST_UNREACHABLE, "SOCKS5 Host unreachable");
                 break;
-            case 5:
-                pce = new ProxyConnectionException(5, "SOCKS5 Connection refused");
+            case CONNECTION_REFUSED:
+                pce = new ProxyConnectionException(CONNECTION_REFUSED, "SOCKS5 Connection refused");
                 break;
-            case 6:
-                pce = new ProxyConnectionException(6, "SOCKS5 TTL expired");
+            case TTL_EXPIRED:
+                pce = new ProxyConnectionException(TTL_EXPIRED, "SOCKS5 TTL expired");
                 break;
-            case 7:
-                pce = new ProxyConnectionException(7, "SOCKS5 Command not supported");
+            case CMD_NOT_SUPPORTED:
+                pce = new ProxyConnectionException(CMD_NOT_SUPPORTED, "SOCKS5 Command not supported");
                 break;
-            case 8:
-                pce = new ProxyConnectionException(8, "SOCKS5 Address type not supported");
-                break;
-            case 9:
-                pce = new ProxyConnectionException(9, "SOCKS5 to X'FF' unassigned");
+            case ADDR_TYPE_NOT_SUPPORTED:
+                pce = new ProxyConnectionException(ADDR_TYPE_NOT_SUPPORTED, "SOCKS5 Address type not supported");
                 break;
             default:
                 pce = new ProxyConnectionException(-1, "SOCKS5 unknown proxy response");
                 break;
+        }
+        
+        if (pce != null) {
+            try {
+                out.close();
+                in.close();
+                this.socket.close();
+            }catch(IOException ioe) {}
+            throw pce;
         }
         
         return this.socket;
@@ -162,53 +203,66 @@ public class Socks5 implements Proxy {
     	
     	requestPacket[0] = 1;
     	requestPacket[1] = userLen;
+        
     	// adding the username to the packet
         System.arraycopy(getUser().getBytes(), 0, requestPacket, 2, userLen);
         requestPacket[2 + userLen] = passLen;
-    	// adding the password to the packet
+    	
+        // adding the password to the packet
         System.arraycopy(getPass().getBytes(), 0, requestPacket, 3 + userLen, passLen);
         
+        OutputStream out = null;
+        InputStream in = null;
         try {
-            this.socket.getOutputStream().write(requestPacket, 0, 3 + userLen + passLen);
-            this.socket.getInputStream().read(response, 0, 2);
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+            
+            out.write(requestPacket, 0, 3 + userLen + passLen);
+            in.read(response, 0, 2);
         }catch(IOException ioe) {
             throw new ProxyConnectionException(-2, "SOCK5 - IOException: " + ioe.getMessage());
         }
         
         ProxyConnectionException pce = null;
         switch (response[1]) {
-            case 0:
-            	return true; // auth successfull
-            case 1:
-                pce = new ProxyConnectionException(1, "SOCKS5 general SOCKS server failure");
+            case REQUEST_OK:
+                return true;// request successfull
+            case GENERAL_FAILURE:
+                pce = new ProxyConnectionException(GENERAL_FAILURE, "SOCKS5 general SOCKS server failure");
                 break;
-            case 2:
-                pce = new ProxyConnectionException(2, "SOCKS5 connection not allowed by ruleset");
+            case NOT_ALLOWED:
+                pce = new ProxyConnectionException(NOT_ALLOWED, "SOCKS5 connection not allowed by ruleset");
                 break;
-            case 3:
-                pce = new ProxyConnectionException(3, "SOCKS5 Network unreachable");
+            case NET_UNREACHABLE:
+                pce = new ProxyConnectionException(NET_UNREACHABLE, "SOCKS5 Network unreachable");
                 break;
-            case 4:
-                pce = new ProxyConnectionException(4, "SOCKS5 Host unreachable");
+            case HOST_UNREACHABLE:
+                pce = new ProxyConnectionException(HOST_UNREACHABLE, "SOCKS5 Host unreachable");
                 break;
-            case 5:
-                pce = new ProxyConnectionException(5, "SOCKS5 Connection refused");
+            case CONNECTION_REFUSED:
+                pce = new ProxyConnectionException(CONNECTION_REFUSED, "SOCKS5 Connection refused");
                 break;
-            case 6:
-                pce = new ProxyConnectionException(6, "SOCKS5 TTL expired");
+            case TTL_EXPIRED:
+                pce = new ProxyConnectionException(TTL_EXPIRED, "SOCKS5 TTL expired");
                 break;
-            case 7:
-                pce = new ProxyConnectionException(7, "SOCKS5 Command not supported");
+            case CMD_NOT_SUPPORTED:
+                pce = new ProxyConnectionException(CMD_NOT_SUPPORTED, "SOCKS5 Command not supported");
                 break;
-            case 8:
-                pce = new ProxyConnectionException(8, "SOCKS5 Address type not supported");
-                break;
-            case 9:
-                pce = new ProxyConnectionException(9, "SOCKS5 to X'FF' unassigned");
+            case ADDR_TYPE_NOT_SUPPORTED:
+                pce = new ProxyConnectionException(ADDR_TYPE_NOT_SUPPORTED, "SOCKS5 Address type not supported");
                 break;
             default:
                 pce = new ProxyConnectionException(-1, "SOCKS5 unknown proxy response");
                 break;
+        }
+        
+        if (pce != null) {
+            try {
+                out.close();
+                in.close();
+                this.socket.close();
+            }catch(IOException ioe) {}
+            throw pce;
         }
         
         return false;
@@ -216,8 +270,8 @@ public class Socks5 implements Proxy {
     
     private void connectToProxy() throws IOException {
         this.socket = new Socket();
-        this.socket.setSoTimeout(Integer.MAX_VALUE);
-        this.socket.connect((SocketAddress) new InetSocketAddress( InetAddress.getByName(getHost()), getPort()), 20000);
+        this.socket.setSoTimeout(getTimeout());
+        this.socket.connect((SocketAddress) new InetSocketAddress( InetAddress.getByName(getHost()), getPort()), getTimeout());
     } 
     
     public Socket bind(InetSocketAddress isa) throws IOException {
@@ -253,7 +307,10 @@ public class Socks5 implements Proxy {
      * @param port The port to set.
      */
     public void setPort(int port) {
-        this.port = port;
+        if (port > 0)
+            this.port = port;
+        else
+            this.port = DEFAULT_PORT;
     }
 
 
@@ -300,23 +357,21 @@ public class Socks5 implements Proxy {
     public void setPrimaryConnectionPort(int primaryConnectionPort) {
         this.primaryConnectionPort = primaryConnectionPort;
     }
-    
-    public static void main (String args[]) throws Exception {
-        Socks5 proxy = new Socks5("127.0.0.1",1080, "anonymous","test");
-        Socket socket = null;
-        
-        try {
-            socket = proxy.connect("172.25.12.195", 23);
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-        
-        System.out.println("looks like we are connected to the proxy ...");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String line;
-        while ( (line = in.readLine()) != null) {
-            System.out.println(line);
-        }
+    /**
+     * @return Returns the timeout.
+     */
+    public int getTimeout() {
+        return timeout;
+    }
+
+    /**
+     * @param timeout The timeout to set.
+     */
+    public void setTimeout(int timeout) {
+        if (timeout > 0)
+            this.timeout = timeout;
+        else
+            this.timeout = DEFAULT_TIMEOUT;
     }
 }
